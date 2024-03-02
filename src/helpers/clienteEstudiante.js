33e77dfed81511ef06c893796f3f058453cf9030
Juan Estrella <jcestrella@uce.edu.ec>
2024-03-01 20:30:13 -0500

    token

--- src/helpers/clienteEstudiante.js
+++ src/helpers/clienteEstudiante.js
@@ -1,19 +1,24 @@
 import axios from "axios";
 
+const token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJIb2xhIG11bmRvIiwiaWF0IjoxNzA5MzQyOTUzLCJleHAiOjE3MDkzNDMwNTN9.cy9VSQFS6V-yl5PY2O9odtjFLhAvjSqnW3lT-1MZ2Xca1nJK4d7Roj15g24HuSklExGiQfEyRJ1hl1Kgc9OHYA';
+
 const consultar = async () => {
-    return axios.get('http://localhost:8089/API/v1.0/Inscripcion/estudiantes').then(r => r.data)
+    const config = {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    };
+    return axios.get('http://localhost:8089/API/v1.0/Inscripcion/estudiantes', config).then(r => r.data)
 }
 
 const insertar = async (body) => {
-    const token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJIb2xhIG11bmRvIiwiaWF0IjoxNzA5MzM4Nzk5LCJleHAiOjE3MDkzMzg4OTl9.ZabVoP1Gw7rp2wLjowhCwecP4MLLtNXykXUheTHbMkjfFKmj3hvxQR1gE_8HAlzb7aIrS8nF_zt86xpQcj-E0g';
-
     const config = {
         headers: {
             'Authorization': `Bearer ${token}`
         }
     };
 
-    const data = axios.post(`http://localhost:8089/API/v1.0/Inscripcion/estudiantes`, body).then(r => r.data)
+    const data = axios.post(`http://localhost:8089/API/v1.0/Inscripcion/estudiantes`, body, config).then(r => r.data)
     console.log(data);
 }
 
